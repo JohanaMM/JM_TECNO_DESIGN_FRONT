57fdc4eccde884d928d03b4f796cc6bc9320aa2c
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-09-19 20:01:23 -0300

    primer commit

--- src/jm_TD.js
+++ src/jm_TD.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import inicio from './paginas/inicio';
-import prueba from './Componentes/prueba';
-
-import './App.css';
+import Inicio from './paginas/inicio'; 
 
 function JM_TD() {
   return (
     <>
-    <Routes>
-    <Route path="/Inicio" element={<inicio/>}/>
-    <Route path="/Inicio" element={<prueba/>}/>
-    </Routes>
+      <Routes>
+      <Route path="/*" element={<Inicio />} /> 
+        <Route path="/Inicio" element={<Inicio />} /> 
+      </Routes>
     </>
   );
 }
 
 export default JM_TD;
+
--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 function inicio() {
   return (
     <>
@@ -6,4 +7,4 @@ function inicio() {
     </>
   );
 }
-export default inicio;
+export default inicio
