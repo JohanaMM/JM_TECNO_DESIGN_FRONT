3fc3640eb70edac9c598f9ff81965b5cb55898e5
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-10-04 17:51:33 -0300

    links

--- src/JM_TD.js
+++ src/JM_TD.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './Componentes/navbar'
 import Inicio from './paginas/inicio'
 import Categorias from './paginas/categorias';
 import Productos from './paginas/productos';
@@ -12,6 +13,7 @@ import Perfil from './paginas/perfil';
 function JM_TD() {
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route path="/*" element={<Inicio />} />
         <Route path="/Inicio" element={<Inicio />} />
